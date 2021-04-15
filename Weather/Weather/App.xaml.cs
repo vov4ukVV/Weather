7e18fd22760d713cc343963d6a52263c0f0a38bd
vov4ukVV <57673496+vov4ukVV@users.noreply.github.com>
2021-04-15 23:43:39 +0300

    Add project files.

---
+++ Weather/Weather/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Weather.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Weather
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new CurrentWeather();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
